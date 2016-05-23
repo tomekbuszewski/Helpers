6a3fadb8ae3a69a69bdd20762f5033fbb716f4f6
Tomek Buszewski <tomek.buszewski@gmail.com>
2016-05-23 08:29:36 +0200

    Better scrolling on fixed

--- babel/index.js
+++ babel/index.js
@@ -2,9 +2,10 @@ require('../sass/style.scss');
 
 function translate(item) {
   const doc = document.documentElement;
-  let scroll = (window.window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  const scroll = (window.window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+  const dist = item.getBoundingClientRect().top;
 
-  item.style.transform = `translate3d(0, ${scroll/2}px, 0)`;
+  item.style.transform = `translate3d(0, ${-scroll}px, 0)`;
 }
 
 import _modals from './components/modals/modals';
@@ -12,4 +13,15 @@ import _modals from './components/modals/modals';
 import _tabs from './components/tabs/tabs';
 
 import _scrolling from './components/scrolling/scrolling';
+
+let parentHeight = 0;
+Array.prototype.forEach.call(document.querySelectorAll('.scroll__element'), (i) => {
+  parentHeight += i.getBoundingClientRect().height;
+  i.style.top = `${i.getBoundingClientRect().top}px`;
+  setTimeout(() => {
+    i.style.position = 'fixed';
+    i.parentNode.style.height = parentHeight+'px';
+  });
+});
+
 _scrolling.add(document.querySelectorAll('.scroll__element'), function() { translate(this.item); });
